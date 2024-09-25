8d2415703ab077e98c59039e01121c64b613bcd2
m.besh <muhammed.beshir@ethiotelecom.et>
2024-09-25 15:13:04 +0300

    Add post

--- src/assets/SelfTest.jsx
+++ src/assets/SelfTest.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SelfTest = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
   return (
     <div className="w-[400px] h-[500px] bg-slate-900 rounded-3xl flex flex-col">
       <div className="bg-slate-300 w-full h-[50px] flex items-center justify-center rounded-t-3xl">
@@ -11,16 +14,30 @@ const SelfTest = () => {
         <input
           className="px-4 py-1.5 rounded-md bg-slate-300 placeholder-slate-600"
           type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
           placeholder="write your mind here...."
         />
-        <button className="bg-slate-300 text-center text-base px-4 py-1.5 rounded-md font-semibold">
+        <button
+          className="bg-slate-300 text-center text-base px-4 py-1.5 rounded-md font-semibold"
+          onClick={() => {
+            setPosts([inputValue, ...posts]);
+            setInputValue("");
+          }}
+        >
           Add +
         </button>
       </div>
 
       <div className="bg-slate-900 flex-1 pt-2 pr-4 pb-4 pl-4 flex">
         <div className="bg-slate-600 flex-1 rounded-t-3xl px-3 py-8">
-          <div className="bg-white">hhhhh</div>
+          <ul className="space-y-1">
+            {posts.map((post, i) => (
+              <li className="bg-yellow-100" key={i}>
+                {post}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
